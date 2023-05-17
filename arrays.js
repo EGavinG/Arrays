2412d1f82707c59e4e7422981874dbe5749accc9
Gavin Garcia <edwardgavingarcia@yahoo.com>
2023-05-17 12:34:30 -0500

    Declare Variables of 4 Arrays

---
+++ arrays.js
@@ -0,0 +1,8 @@
+//Declare a variable that stores an Array of at least 4 Strings
+var names = ["Gavin", "Noah", "Renton", "Mom",];
+
+//Declare a variable that stores an Array of at least 4 Numbers
+var luckyNumbers = [4, 11, 93, 2];
+
+//Declare a variable that stores an Array of at least 4 Booleans
+var booleans = [true, false, true, false];
