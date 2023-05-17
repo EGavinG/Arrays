7110d5e93cf2cfde512f61c0f835520c763144e5
Gavin Garcia <edwardgavingarcia@yahoo.com>
2023-05-17 12:56:47 -0500

    Call each Declared Array Variable above

--- arrays.js
+++ arrays.js
@@ -6,3 +6,24 @@ var luckyNumbers = [4, 11, 93, 2];
 
 //Declare a variable that stores an Array of at least 4 Booleans
 var booleans = [true, false, true, false];
+
+//Call method for an Array of Strings
+console.log(names[2]);
+//This call will result in "Renton" being printed
+//The call states to pull the second value in the array of strings
+//Index position states 0 is the initial start so starting value in the string array
+//is "Gavin", 1 is "Noah", and 2 is "Renton"
+
+//Call Method for an Array of Numbers
+console.log(luckyNumbers[0]);
+//This call will result in "4" being printed
+//The call states to pull the initial value in the array of numbers
+//Index position states 0 is the initial start so starting value in the number array 
+//is "4"
+
+//Call Method for an Array of Booleans
+console.log(booleans[3]);
+//This call will result in "false" being printed
+//This call states to pull the third value in the array of booleans
+//Index position states 0 is the intitial start so starting value in the boolean array
+//is "true", 1 is "false", 2 is "true", and 3 is "false"
